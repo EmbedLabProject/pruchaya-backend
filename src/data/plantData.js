a6e11c047b99f2ab4db9aad629157bd6b111eec1
Bo0K31 <ahtusaw@gmail.com>
2024-05-18 20:49:41 +0700

    change send data to file, add error handler, add test image

--- src/data/plantData.js
+++ src/data/plantData.js
@@ -1,20 +1,30 @@
 import FormData from "form-data";
 
-const api_key = "2b10Nd0zDY95NF6n5C9YrCCfe"
+export async function getSpecies(file) {
+    
+    const project = 'all'; // Change to a specific flora if needed
+    const apiKey = '2b10kVnMfoV1NWbG3rJWbp3M7'; // Replace with your actual API key
 
-export async function getSpecies(form) {
-    const project = 'all'
-
-    const url = `https://my-api.plantnet.org/v2/identify/${project}?api-key=${api_key}`;
-
-    let newForm = new FormData(form)
-    console.log(newForm)
-
-    const {status,data} = await fetch(url, {
-        method: "POST",
-        body: newForm
+    let form = new FormData();
+    req.files.forEach(file => {
+      form.append('images', fs.createReadStream(file.path));
     });
 
-    console.log('status', status); // should be: 200
-    console.log('data', data); // should be: read "Step 6" below
+    const { status, data } = await axios.post(
+        `https://my-api.plantnet.org/v2/identify/${project}?api-key=${apiKey}`,
+        form,
+        {
+          headers: form.getHeaders()
+        }
+      );
+
+    if (status === 200 && data.results && data.results.length > 0) {
+        const scientificName = data.results[0].species.scientificNameWithoutAuthor;
+        console.log('scientificName: ',scientificName)
+    } else {
+        res.status(404).send('No plant identification results found.');
+    }
+
+    // console.log('status', status); // should be: 200
+    // console.log('data', data); // should be: read "Step 6" below
 }
