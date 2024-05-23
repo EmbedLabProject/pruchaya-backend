e84c9e453a1df1aa64be1b44df5d96860defb7be
ttontoey <65761088+ttontoey@users.noreply.github.com>
2024-05-23 17:39:28 +0700

    add test

--- src/data/sensorData.js
+++ src/data/sensorData.js
@@ -1,5 +1,5 @@
 let allSensorData = [];
-allSensorData.push({device_id: "Test", latestFetch: 0, data: [{light:79, humidity: 32, vibration: 48, time: 1716434158},{light:79, humidity: 33, vibration: 51, time: 1716434300}]})
+allSensorData.push({device_id: "Test", latestFetch: 0, data: [{light: 10, humidity: 10, vibration: 10, time: 1716434158},{light: 20, humidity: 20, vibration: 20, time: 1716434158},{light: 30, humidity: 30, vibration: 30, time: 1716434158},{light: 40, humidity: 40, vibration: 40, time: 1716434158},{light: 50, humidity: 50, vibration: 50, time: 1716434158},{light: 60, humidity: 60, vibration: 60, time: 1716434158},{light: 70, humidity: 70, vibration: 70, time: 1716434158},{light: 80, humidity: 80, vibration: 80, time: 1716434158},{light: 90, humidity: 90, vibration: 90, time: 1716434158},{light: 100, humidity: 100, vibration: 100, time: 1716434158},{light: 80, humidity: 0, vibration: 0, time: 1716434158},{light: 0, humidity: 80, vibration: 0, time: 1716434158},{light: 0, humidity: 0, vibration: 80, time: 1716434158}]})
 
 
 
